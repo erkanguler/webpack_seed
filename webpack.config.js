3580cf24a6c59f6461035caf3b223ce629529cc9
Erkan Güler <erkan@tilf.se>
2018-02-19 09:05:26 +0100

    Update

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,20 @@
 var webpack = require('webpack');
+
 module.exports = {
-  entry: './main.js',
-  output: {
-    filename: 'bundle.js'
-  },
-  plugins: [
-    new webpack.NamedModulesPlugin(),
-    new webpack.HotModuleReplacementPlugin()
-  ],
-  devServer: {
-    inline: true,
-    hot: true
-  }
+    entry: './main.js',
+    output: {
+        filename: 'bundle.js'
+    },
+    plugins: [
+        new webpack.NamedModulesPlugin(),
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    devServer: {
+        inline: true,
+        hot: true
+    },
+    watchOptions: {
+        aggregateTimeout: 300,
+        poll: 1000
+    }
 };
